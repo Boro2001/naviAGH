dac65f8b666c16ea247f29b277e58eb20be8c265
plgborowiczm <mikolajborowicz7@gmail.com>
2023-10-14 16:00:45 +0200

    Initial Commit

---
+++ naviAGH/naviAGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  naviAGHApp.swift
+//  naviAGH
+//
+//  Created by Mikołaj Borowicz on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct naviAGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
